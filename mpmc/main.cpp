039276c3b57219dafb7808244c65e502d5b95eda
jack.shi <jack.shi@teletracnavman.com>
2026-04-14 09:43:52 +1200

    adding test for mpmc

--- mpmc/main.cpp
+++ mpmc/main.cpp
@@ -1,11 +1,34 @@
 #include "log.h"
+#include <thread>
+
+const size_t test_count = 1000000;
+
+void test_func_1(void)
+{
+   for(size_t i = 0; i < test_count; i++)
+   {
+     LOG_INFO("This is %s and the count is %lu", __FUNCTION__, i);
+   }
+}
+
+void test_func_2(void)
+{
+   for(size_t i = 0; i < test_count; i++)
+   {
+     LOG_INFO("This is %s and the count is %lu", __FUNCTION__, i);
+   }
+}
 
 
 int main(void)
 {
   Log::instance()->init(LogLevel::INFO);
-  
-  LOG_INFO("hello world!!!");
+  std::thread t1(test_func_1);
+  std::thread t2(test_func_2);
+  t1.join();
+  t2.join();
+
+
   Log::instance()->log_close();
   return 0;
 }
